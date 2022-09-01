9e502e5ae1cbcb1098dfb624836bd1410aa2f363
anuLoveCoading <anusrivas07@gmail.com>
2022-09-01 13:08:59 +0530

    first commit

---
+++ 01.js
@@ -0,0 +1,96 @@
+"use strict";
+
+// console.log can be print something on console
+// console.log("INDIA",`:`,`HINDUSTAN`);
+
+// intro a variable
+// variable can store some information
+// we can use that information later
+// we  can change that information later
+
+// declare a variable
+
+
+// use a variable
+//   let first_name ="Anurag"
+// console.log(first_name);
+// console.log(first_name.toUpperCase(),`:`,first_name.toLowerCase())
+
+// change value
+//  first_name="anurag"
+// console.log(first_name)
+
+
+
+
+
+// find the square root of any nuber 
+// let x=16
+// console.log(x**0.5)
+//find the square of any number
+// console.log(5**2)
+
+
+// find the cube root of any number
+// let x= 9**3
+// console.log(Math.cbrt(x))
+//find the cube of any number
+//console.log(5**3)
+
+
+// for (let i=1;i<=10;i++){
+//     let bag=""
+//      for(let j=1;j<=i;j++){
+//         bag+="*"+" "
+//     }console.log(bag)
+// }
+
+
+
+// ********************VARIABLE**********************
+
+// rules for naming variables
+//you cannot start with number
+//example:- 
+// 1value(invalid)
+//value1(valid)
+
+
+
+
+// you can use only undersore _ or dollar symbol
+// first_name (valid)
+// _firstname(valid)
+
+// first$name (vaild)
+// $firstname (vaild)
+
+// you cannot use spaces
+// first name (Invaild)
+
+// convection
+// start with small letter and use camelCase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
