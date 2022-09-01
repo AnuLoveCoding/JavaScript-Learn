43cf372f46dbbf65ff32456b8290f4cbb0253e24
anuLoveCoading <anusrivas07@gmail.com>
2022-09-01 13:42:51 +0530

    02 class

--- 02.js
+++ 02.js
@@ -4,13 +4,25 @@ console.log("ANURAG KUMAR");
 //  let keyword
 // declare variable with let keyword
 
-let firstName ="Anurag"
-console.log(firstName);
+// let firstName ="Anurag"
+// console.log(firstName);
 
 // block scope vs function scope (covered latter in this videos.)
 
 //  declare constant
-  const pi = 3.14;
+//   const pi = 3.14;
 //   pi =3.15;
-  console.log(pi)
+//   console.log(pi)
 
+// string indexing;
+//  let firstName = "Anurag"
+//  console.log(firstName[3]);
+//  console.log(firstName.length);
+//  console.log(firstName.length-1);
+//  console.log(firstName[firstName.length-4])
+
+
+//  trim()
+//  toUppercase()
+// toLowercase()
+//  slice
