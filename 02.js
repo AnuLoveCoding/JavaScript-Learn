9766d7c546afd657b17c168df06f62b095034346
anuLoveCoading <anusrivas07@gmail.com>
2022-09-01 13:17:35 +0530

    01 class

--- 02.js
+++ 02.js
@@ -1,1 +1,12 @@
+"use strict";
 console.log("ANURAG KUMAR");
+
+//  let keyword
+// declare variable with let keyword
+
+let firstName ="Anurag"
+console.log(firstName);
+
+// block scope vs function scope (covered latter in this videos.)
+
+
