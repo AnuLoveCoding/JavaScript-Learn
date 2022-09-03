9cb7b0ce1b9325d389edccb3652c53d14b979f09
anuLoveCoading <anusrivas07@gmail.com>
2022-09-03 21:42:47 +0530

    05 classs

--- 05.js
+++ 05.js
@@ -1,3 +1,48 @@
+//  intro to array
+// refrence type
+
+// how to create arrays
+
+//  let fruits =['apple','mango','fruit','gauva','coconut']
+//  console.log(fruits[1])
+
+
+
+
+// aaray indexing
+
+
+// let fruits =['apple','mango','fruit','gauva','coconut']
+// fruits[0]='banana'
+// console.log(fruits)
+
+// console.log(typeof fruits) // object 
+// array is a object type.
+// console.log(Array.isArray(fruits)) // checking array is not in this      method
+
+//    let obj={}  // object literal
+
+
+
+//  array push 
+//  array  shift unshift
+
+let fruits =['apple','mango','fruit','gauva','coconut']
+// push
+ fruits.unshift('pine-apple',`Ikkah`)// unshift is a function  to add in array in start in array
+
+
+//  shift
+
+ console.log(fruits.shift()) // shift is a function to substract in array in start in array.
+
+// fruits.push('banana')
+// console.log(fruits)
+console.log(fruits)
+  let fruit1=fruits.pop()
+ console.log(`fruit1 is stored : ${fruit1}`)
+
+
 
 
 
