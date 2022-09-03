a9d5e51bad2c4c6d1ba70207f3f4e98de684822f
anuLoveCoading <anusrivas07@gmail.com>
2022-09-03 22:04:02 +0530

    04 class

---
+++ 06.js
@@ -0,0 +1,29 @@
+// console.log(`hello`)
+
+// primitive vs refrence data types
+
+
+
+let num1 =6;
+num2=num1
+console.log(`the value of num1 is ${num1}`)
+console.log(`the value of num2 is ${num2}`)
+num1++;
+console.log("*****after the incrementing the num1******")
+console.log("***************")
+console.log(`the value of num1 is ${num1}`)
+console.log(`the value of num2 is ${num2}`)
+
+console.log(`#########################################`)
+// refrence type
+// array
+ let array1=[`item1`,'item2']
+ array2=array1
+ console.log(`the value of array1 is ${array1}`)
+ console.log(`the value of array2 is ${array2}`)
+ array1.push('item3')
+ console.log("after push in array *************")
+ console.log(`the value of array1 is ${array1}`)
+ console.log(`the value of array2 is ${array2}`)
+
+ 
