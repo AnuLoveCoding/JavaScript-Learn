431ee797255ad509d3c7bdc3c0e01a249843195f
anuLoveCoading <anusrivas07@gmail.com>
2022-09-03 23:45:49 +0530

    07 class

--- 07.js
+++ 07.js
@@ -1,2 +1,46 @@
 // for loop in array
 
+// let fruit =["apple","mango","grapes","banana"]
+// console.log(fruit)
+// console.log(fruit[0])
+// console.log(fruit[2])
+// console.log(fruit.length)
+
+
+// let fruit2=[]
+
+// for( let i=0; i<=fruit.length-1;i++){
+//        fruit2.push(fruit[i].toUpperCase())
+// }
+
+// console.log(fruit2)
+
+
+
+
+
+// use const for creating array
+
+// const pi= 3.14
+// console.log(pi)
+// pi=5.12
+
+//  90%  developer use const in array.
+
+
+// heap memory
+
+// const fruits =["apple"," mango"]
+// fruits.push("banana")
+// console.log(fruits)
+
+
+//  while loop in array
+const fruits =["apple"," mango","banana"]
+
+let i=0;
+while(i<fruits.length){
+    console.log(fruits[i])
+    i++
+}
+
