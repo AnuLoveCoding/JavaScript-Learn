18e988a31b7040085f77de3ca8ad111db7006752
anuLoveCoading <anusrivas07@gmail.com>
2022-09-03 20:10:32 +0530

    04 class

---
+++ 04.js
@@ -0,0 +1,72 @@
+//  nested if else
+
+// winning number 19;
+
+// 19 your guess us rught
+// 17 too low
+// 20 too high
+
+
+// let winningNumber = 10
+// let userGuess = +prompt("Guess a number") // use for security check prompt
+
+
+// if(userGuess==winningNumber){
+//     console.log("Your guess is right")
+// }else{
+//     if(userGuess < winningNumber){
+//         console.log("too low!!")
+//     }else{
+//         console.log("too high!!!")
+//     }
+// }
+
+
+// if 
+// else if
+// else if
+// else
+
+// let tempreature = 16;
+
+
+// if (tempreature <0){
+//     console.log("extremely cold outside")
+// }else if( tempreature< 16){
+//     console.log("It is cold outside")
+// }else if (tempreature< 25){
+//     console.log("wheather is okay")
+// } else if (tempreature<35){
+//     console.log("Let's go to Swim")
+// }else if(tempreature<45){
+//     console.log("turn on A/C")
+// }else{
+//     console.log("too hot")
+// }
+
+// console.log ("Hello Anurag")
+
+
+// switch statement 
+
+
+let day =  5
+
+
+switch(day){
+    case 1: console.log("sunday")
+           break;
+    case 2: console.log("Monday")
+         break;
+    case 3: console.log("Tuesday")
+              break;
+    case 4: console.log("Wednesday")
+         break
+    case 5: console.log("Thrusday")
+         break
+    case 6 : console.log("Friday")
+         break
+    case 7 : console.log("Saturday")
+          break
+    default : console.log("!Sorry, try ")
+}
