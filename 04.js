df49f6acfa033446fc554af77f63148fac9270c7
anuLoveCoading <anusrivas07@gmail.com>
2022-09-03 21:08:11 +0530

    04 class

--- 04.js
+++ 04.js
@@ -101,6 +101,23 @@
 // console.log(`hello word ${i}`)
 
 
+// do while loop
+
+
+
+//  let i=0
+// while (i<=9){
+//     console.log(i)
+//     i++
+// }
+
+// let i=10;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=9)
+
+// console.log(`value of i is ${i}`)
 
 
 
