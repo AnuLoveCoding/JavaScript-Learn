dde750594bcc33861dd4ab0479f351544fdccfd2
anuLoveCoading <anusrivas07@gmail.com>
2022-09-04 00:02:19 +0530

    08 class

--- 08.js
+++ 08.js
@@ -9,4 +9,14 @@
 
 //  for of loop in array
 
+const fruits =["apple","mango","banana"]
+//  for( let fruit of fruits){
+//     console.log(fruit.toUpperCase())
+//  }
 
+
+//index give me array index number
+
+ for (let index in fruits){  
+    console.log( fruits[index])
+ }
