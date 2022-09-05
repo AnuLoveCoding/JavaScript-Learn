9f65eead0e18bf4498399f4bfe18433ba7b58c8d
anuLoveCoading <anusrivas07@gmail.com>
2022-09-05 09:22:45 +0530

    09 class

---
+++ 09.js
@@ -0,0 +1,77 @@
+// console.log("hello")
+
+
+// how to iterate object
+
+// const person ={
+//         name : "Anurag",
+//         age  : 21,
+//          "me hobbies" : ["guitar","slleping","chess"]
+//      }
+
+//      person.gender= "Male"
+//      person.location= "Bihar"
+//      person.friends= "True"
+//        person.girlfriend ="NO"
+    //   for in loop
+    // object.keys
+
+    //  for(let key in person){
+    //     console.log(`${key} : ${person[key]}`)
+    //  }
+
+
+
+    //  for (let i=1;i<=person;i++){
+    //     console.log(person[i])
+    //  }
+
+    // console.log(typeof(Object.keys(person)));
+    // let tr= Array.isArray((Object.keys(person)))
+    // console.log(tr)
+
+//  ******************computed properties*************
+
+
+
+
+
+// spread operator
+
+// const array1=[1,2,3,4];
+// const array2=[5,6,7,8];
+
+
+// let newarray=[...array1,...array2,69,70,71]
+// console.log(newarray)
+
+// let add1=[..."45212578793"]
+// console.log(add)
+
+//   let sum=add
+  
+
+// for(let add of add1){
+//     console.log(add)
+// }
+
+
+
+// const obj1 ={
+//     key1: "Anurag",
+//     key2 : "value2",
+// }
+
+// const obj2={
+//     key3 : 'value1',
+//     key4 : "value2",
+// }
+
+// let newObj ={...obj1,...obj2,key70: "value6"}
+// console.log(newObj)
+
+
+
+
+
+
