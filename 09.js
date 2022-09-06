1d1830307502095c7a2212e75da808816d57403e
anuLoveCoading <anusrivas07@gmail.com>
2022-09-06 23:52:53 +0530

    09 class

--- 09.js
+++ 09.js
@@ -35,23 +35,23 @@
     // console.log(tr)
 
 //  ******************computed properties*************
-const key1 ="objkey1";
-const key2 ="objkey2";
+// const key1 ="objkey1";
+// const key2 ="objkey2";
 
-const value1="myvalue1";
-const value2="myvalue2";
+// const value1="myvalue1";
+// const value2="myvalue2";
 
-// const Obj={
-//     objkey1 ="myvalue1";
-//     objkey2=" myvalue2";
+// // const Obj={
+// //     objkey1 ="myvalue1";
+// //     objkey2=" myvalue2";
 
+// // }
+
+// const obj ={
+//     [key1]: value1,
+//     [key2] : value2,
 // }
-
-const obj ={
-    [key1]: value1,
-    [key2] : value2,
-}
-console.log(obj);
+// console.log(obj);
 
 
 
