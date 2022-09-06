d567b91d7620752617b10467718585866db95165
anuLoveCoading <anusrivas07@gmail.com>
2022-09-07 00:39:54 +0530

    10 class

---
+++ 10.js
@@ -0,0 +1,36 @@
+// object Destructuring
+
+
+// const band ={
+//     bandName : "led zepplin",
+//     famousSong:" stairway to heaven",
+//     year:1968,
+//     anotherFamousSong:"zilkka",
+//     name1:"anurag"
+// }
+
+// const {bandName:var1,famousSong:var2,...resprops} = band;
+// console.log(var1,var2);
+// console.log(resprops);
+
+
+// *****************Object Inside Array***************
+// Very useful in real world application.
+
+
+// const users = [
+//     {userId:1,firstName:"Anurag",gender:"male"},
+//     {userId:2,firstName:"Ravi",gender:"male"},
+//     {userId:3,firstName:"Sony",gender:"female"},
+//     {userId:4,firstName:"Mahesh",gender:"male"},
+// ]
+// console.log(users);
+
+// for (let user of users){
+//     console.log(user);
+// }
+// const [{firstName:name},user2,{gender},user4]=users;
+
+// const [user1,user2,user3,user4]=users;
+// console.log(name);
+// console.log(gender);
