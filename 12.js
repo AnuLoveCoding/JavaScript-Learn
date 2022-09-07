a9ab4b433460b1060777e99076c2836c1f27b17b
anuLoveCoading <anusrivas07@gmail.com>
2022-09-07 15:58:25 +0530

    12 class Array

--- 12.js
+++ 12.js
@@ -1,1 +1,24 @@
-console.log("objects");
+// arrow function;
+
+
+//  const mybirthday=()=>{
+//     console.log("Anurag");
+//     console.log("anurag");
+//  }
+//  mybirthday();
+
+
+// const sumThreeValue=(number1,number2,number3)=>{
+//     return number1+number2+number3;
+// }
+
+//  console.log(sumThreeValue(55,200,2));
+// //  console.log(a);
+
+
+const isEven=(number)=>{
+    return number%2==0
+    return false;
+}
+
+console.log(isEven(2));
