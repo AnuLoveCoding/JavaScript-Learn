08da7568c3c884547119fa9f051036ffa8096df0
anuLoveCoading <anusrivas07@gmail.com>
2022-09-07 21:56:59 +0530

    15 class

--- 15.js
+++ 15.js
@@ -1,8 +1,27 @@
 // callback function;
 
-function myfunc(a){
-    console.log(a);
-    console.log(`hello World`);
-}
+// function myfunc(a){
+//     console.log(a);
+//     console.log(`hello World`);
+// }
 
-myfunc({name:"Anurag"});
+// myfunc({name:"Anurag"});
+
+// ********function returning function**********
+
+// function myfunc(){
+//      return "anurag";
+// }
+
+// const ans= myfunc();
+// console.log(ans);
+
+function myfunc(){
+    function hello(){
+        console.log("hello World")
+    }
+    return hello;
+}
+const ans=myfunc();
+ans();
+
