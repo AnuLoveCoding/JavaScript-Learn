9bd317f084d49e130d9d27966b76581a6b5fd3c1
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 17:30:41 +0530

    18 class

---
+++ 18.js
@@ -0,0 +1,24 @@
+//  sort method
+// ASCII Table
+// char : ascii value
+
+
+
+//  sort
+
+// 5,9,1200,400,3000
+//  5,9,400,1200,3000
+
+const numbers = [5,9,1200,400,3000];
+numbers.sort((a,b)=>{
+    return a-b;
+});
+console.log(numbers);
+
+
+// userNames =['harshit','abcd','mohit','nitish','ANURAG'];
+// userNames.sort();
+// console.log(userNames);
+
+
+// const numbers =[5,9,1200,410,3000]
