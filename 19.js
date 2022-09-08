df69aa4bcbc65cc0be5000f4d0d32898bd47eb41
anuLoveCoading <anusrivas07@gmail.com>
2022-09-08 23:24:53 +0530

    19 class

---
+++ 19.js
@@ -0,0 +1,37 @@
+// some method 
+
+// const numbers =[3,5,9,9];
+
+// // kya ek bhi number esa hai jo even hai
+// // true
+
+//  const ans=numbers.some((number)=>number%2==0);
+// console.log(ans);
+
+
+// const userCart =[
+//     {productId:1, productName:"mobile", price:1200},
+//     {productId:2, productName:"laptop", price:2200},
+//     {productId:3, productName:"tv",     price:15000},
+//     {productId:4, productName:"shirt",  price:450},
+//  ]
+
+//  const ans=userCart.some((cartItem)=> cartItem.price>10000);
+//  console.log(ans);
+
+
+//                **************fill method****************
+// value ,start,end
+
+// const myArray =new Array(10).fill(0);
+// console.log(myArray);
+
+//  const myArray =[ 1,2,3,4,5,6,7,8];
+//  myArray.fill(0,2,5);
+//  console.log(myArray);
+
+
+//  splice method
+//  start, delete, insert
+
+const myArray = [ ]
