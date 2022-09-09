61245486c10f653374fde9153c5f18ec93caa888
anuLoveCoading <anusrivas07@gmail.com>
2022-09-09 18:43:07 +0530

    22 class

---
+++ 22.js
@@ -0,0 +1,90 @@
+// console.log(this);
+
+// "use Strict";
+// function myfunc(){
+    
+//     console.log(this);
+// }
+// myfunc();
+
+
+// // 
+// function hello (){
+//       console.log("Anurag");
+// }
+
+// hello.call();
+
+// call apply bind 
+
+
+// const user1={
+//     firstName : "Anurag",
+//     age: 21,
+//     about : function (){
+//         console.log(this.firstName , this.age);
+//     }
+// } 
+
+// const user2 ={
+//     firstName : "Mohit",
+//     age: 22,
+
+// }
+
+// user1.about(user1);
+
+//  don't do this mistake
+
+
+//  proto , prototype ,class
+
+//  const user ={
+//     firstName : "Anurag",
+//     lastname : 'Mundra',
+//     email : "anusrivas07@gmail.com",
+//     age : 22,
+//     address : "house number ,colony, pincode ,state ",
+//     about : function(){
+//         return `${this.function} is ${this.age} years,`
+
+//     },
+
+//     is18 : function(){
+//         return this.age>=18;
+//     }
+//  }
+//  const aboutUser= user.about();
+
+// function (that function create object)
+// 2.) add key value pair
+// 3.) object return karega
+
+
+// function createUser(firstName, lastName, email, age, address){
+//     const user ={};
+//     user.function =firstName;
+//     user.lastName =lastName;
+//     user.email =email;
+//     user.age =age;
+//     user.address =address;
+//     user.about = function(){
+//       return `${this.function} is ${this.age} years`}; 
+
+// }
+
+
+//  this is happening
+
+
+
+
+
+
+
+
+
+
+
+
+
